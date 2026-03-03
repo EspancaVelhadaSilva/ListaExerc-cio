b7c7b7e8b65614c13ebe050aa50d6dda44319871
Arthur M.Pioli <arthur.maximino.oliveira@escola.pr.gov.br>
2026-03-03 11:24:15 -0300

    Refactor voting age program and add even/odd check

--- ex08.js
+++ ex08.js
@@ -1,10 +1,8 @@
-//Criar um programa que exiba as seguintes mensagens: SE menor de 16 anos "não pode votar". SE tiver entre 16 e 18 e maior que 70 anos é "Voto facultativo". SE tiver entre 18 e 70 anos "Voto obrigatório"
-let idade = 10;
+//Programa para verificar se um número é par ou ímpar
+let numero = 6;
 
-if(idade < 16){
-    console.log('Não permitido a votar');
-}else if(idade >= 16 && idade <= 17 || idade > 69){
-    console.log('Voto opcional');
-} else {
-    console.log('Voto obrigatório');
+if (numero % 2 === 0){
+    console.log('O número é par'); 
+} else{
+    console.log('O número é ímpar');
 }
